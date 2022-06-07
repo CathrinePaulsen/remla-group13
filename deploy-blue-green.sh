7cebc295f9622d60ca03607455c7eeed4e5a5292
Cathrine Paulsen <c.r.paulsen@student.tudelft.nl>
2022-06-07 21:27:17 +0200

    ci: allow deploy-blue-green script to also do simple deploy, i.e. new tag is optional

--- deploy-blue-green.sh
+++ deploy-blue-green.sh
@@ -7,14 +7,15 @@ TAG=$2
 IMAGE=ghcr.io/cathrinepaulsen/remla-group13
 DEPLOYMENT_FILE=k8s/$COLOR-deployment.yml
 USAGE=$(cat <<-END
-usage: ${SCRIPT_NAME}    <deployment>    <tag_name>
+usage: ${SCRIPT_NAME}    <deployment>    [tag_name]
 
 arguments:
     <deployment>    green or blue
-    <tag_name>      tag name of the new image, i.e. version
+    [tag_name]      optional: tag name of the new image, i.e. version
 
 description:
     This script updates the version for either the blue or green deployment to the given tag name.
+    If no tag name is given, it will deploy the deployment with its existing image.
     Deployment must be either blue or green.
 END
 )
@@ -25,8 +26,13 @@ case "$COLOR" in
 	*) echo "$USAGE"; exit 1;
 esac
 
-echo "Deploying version $TAG to file $DEPLOYMENT_FILE..."
-sed -i "s?${IMAGE}:.*?${IMAGE}:${TAG}?g" $DEPLOYMENT_FILE
+if [ "$#" -eq 1 ]; then
+	echo "Deploying $COLOR..."
+
+elif [ "$#" -eq 2 ]; then
+	echo "Deploying version $TAG to file $DEPLOYMENT_FILE..."
+	sed -i "s?${IMAGE}:.*?${IMAGE}:${TAG}?g" $DEPLOYMENT_FILE
+fi
 
 kubectl apply -f $DEPLOYMENT_FILE
 echo "Done."
