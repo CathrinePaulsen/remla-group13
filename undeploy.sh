31a33a3ebafcafc0bbe5bd91c9706967a78ddbe1
Cathrine Paulsen <c.r.paulsen@student.tudelft.nl>
2022-06-07 21:30:13 +0200

    ci: add script to quickly undeploy green/blue

---
+++ undeploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SCRIPT_NAME=$0
+COLOR=${1,,}  # Convert input to lowercase
+
+DEPLOYMENT_FILE=k8s/$COLOR-deployment.yml
+USAGE=$(cat <<-END
+usage: ${SCRIPT_NAME}    <deployment>
+
+arguments:
+    <deployment>    green or blue
+
+description:
+    This script undeploys the given deployment to free up system resources.
+    Deployment must be either blue or green.
+END
+)
+
+case "$COLOR" in
+	green) ;;
+	blue) ;;
+	*) echo "$USAGE"; exit 1;
+esac
+
+echo "Undeploying deployment $COLOR..."
+
+kubectl delete -f $DEPLOYMENT_FILE
+
+echo "Done."
