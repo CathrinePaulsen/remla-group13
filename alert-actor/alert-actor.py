5b1468bd75762f9f42deabd26fd8e5444382bc22
Cathrine Paulsen <c.r.paulsen@student.tudelft.nl>
2022-06-11 13:02:47 +0200

    fix: handle firing alerts only

--- alert-actor/alert-actor.py
+++ alert-actor/alert-actor.py
@@ -3,15 +3,19 @@ import subprocess
 
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         print("Data received from Webhook is: ", request.json)
+        if request.json["status"] == "firing":
+            with open('k8s/services.yml') as f:
+                lines = f.read()
+                if "color: blue" in lines:
+                    subprocess.call(['bash',
+                                     './switch-blue-green-traffic.sh',
+                                     'green'])
+    return "Webhook received!"
 
-        with open('k8s/services.yml') as f:
-            lines = f.read()
-            if "color: blue" in lines:
-                    subprocess.call(['bash', './switch-blue-green-traffic.sh', 'green'])
-        return "Webhook received!"
 
 app.run(host='0.0.0.0', port=8081)
